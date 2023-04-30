b5a57d51ae073e7810cc61825dc7abfa31ee607f
cpeppsi <caepps28@gmail.com>
2023-04-30 10:57:51 -0400

    initial commit: set up express, controllers, models

---
+++ controllers/bread.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+//model data variables are typically capital
+const Bread = require('../models/bread')
+
+//GET all bread
+router.get('/', (req, res) => {
+  res.send(Bread)
+})
+
+//GET specific bread
+router.get('/:index', (req, res) => {
+  const { index } = req.params
+  res.send(Bread[index])
+})
+
+module.exports = router
