e2b043b639bd7726e485b11508fd3faa5669d386
cpeppsi <caepps28@gmail.com>
2023-05-02 21:11:49 -0400

    Add new bread

--- controllers/bread.js
+++ controllers/bread.js
@@ -9,6 +9,10 @@ router.get('/', (req, res) => {
   })
 })
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
 //GET specific bread
 router.get('/:index', (req, res) => {
   const { index } = req.params
@@ -16,6 +20,19 @@ router.get('/:index', (req, res) => {
     bread: Bread[index]
   })
   // res.send(Bread[index])
+})
+
+router.post('/', (req, res) => {
+  if(!req.body.image)req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+  if (req.body.hasGluten === 'on') {
+      req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+
+  Bread.push(req.body)
+  // res.send(Bread)
+  res.redirect('/breads')
 })
 
 module.exports = router
