05428eb8dd12edb6f34adf1227cf2464589cb773
cpeppsi <caepps28@gmail.com>
2023-05-04 19:57:31 -0400

    Added edit

--- controllers/bread.js
+++ controllers/bread.js
@@ -18,9 +18,17 @@ router.get('/:index', (req, res) => {
   const { index } = req.params
   res.render('show', {
     bread: Bread[index],
-    index: index
+    index
+    //or index as the key, with index as the value using index: index
   })
-  // res.send(Bread[index])
+})
+
+router.get('/:index/edit', (req, res) => {
+  const { index } = req.params
+  res.render('edit', {
+    bread: Bread[index],
+    index
+  })
 })
 
 router.post('/', (req, res) => {
@@ -40,6 +48,20 @@ router.delete('/:index', (req, res) => {
   const { index } = req.params
   Bread.splice(index, 1)
   res.status(303).redirect('/breads')
+})
+
+router.put('/:index', (req, res) => {
+  const { index } = req.params
+  if(!req.body.image)req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+
+  if (req.body.hasGluten === 'on') {
+      req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+
+  Bread[index] = req.body
+  res.status(303).redirect(`/breads/${index}`)
 })
 
 module.exports = router
